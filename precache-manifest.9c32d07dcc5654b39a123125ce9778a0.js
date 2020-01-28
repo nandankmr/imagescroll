4af2191a8ba490c078f2c78bd41cd8520f48ec5d
Nandan <nandankmrjha@gmail.com>
2020-01-28 13:20:44 +0530

    Updates

---
+++ precache-manifest.9c32d07dcc5654b39a123125ce9778a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a26381aaf8be7e6404ab40442d4c99b",
+    "url": "/imagescroll/index.html"
+  },
+  {
+    "revision": "748e5ed74456dd20e4d3",
+    "url": "/imagescroll/static/css/main.a11087ba.chunk.css"
+  },
+  {
+    "revision": "937ac89318c4b5e8efc3",
+    "url": "/imagescroll/static/js/2.1b33e254.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/imagescroll/static/js/2.1b33e254.chunk.js.LICENSE"
+  },
+  {
+    "revision": "748e5ed74456dd20e4d3",
+    "url": "/imagescroll/static/js/main.24fb68e0.chunk.js"
+  },
+  {
+    "revision": "1bb40c203511c0db3fac",
+    "url": "/imagescroll/static/js/runtime-main.0fc99357.js"
+  }
+]);
